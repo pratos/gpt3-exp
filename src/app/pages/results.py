fb8612def7037d6b2491b326b1c1637e9ef0ed97
Prathamesh Sarang <prathamesh@difference-engine.ai>
2020-08-30 02:58:19 +0530

    chore(streamlit): update results page with nothing

--- src/app/pages/results.py
+++ src/app/pages/results.py
@@ -2,8 +2,9 @@ from pathlib import Path
 
 import streamlit as st
 
-IMAGE_PATH = Path(__file__).parents[2] / "assets"
+IMAGE_PATH = Path(__file__).parents[3] / "assets"
 
 
 def results():
-    st.image(f"{IMAGE_PATH}")
+    st.subheader("Waiting to be implemented...")
+    st.image(f"{IMAGE_PATH / 'waiting.gif'}")
