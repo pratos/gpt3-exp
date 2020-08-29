34dace648dfe347a204ad828373b66f4cbc0ac34
Prathamesh Sarang <prathamesh@difference-engine.ai>
2020-08-30 02:08:40 +0530

    build(scripts): add script to run streamlit server

--- scripts.py
+++ scripts.py
@@ -1,3 +1,22 @@
-import logging
+import subprocess
+from pathlib import Path
 
-logging.info("This")
+import click
+from loguru import logger
+
+THIS_DIRECTORY = Path(__file__).parent
+
+
+@click.command()
+def st_server():
+    logger.info("Starting server...")
+    subprocess.run(
+        ["poetry", "run", "streamlit", "run", str(THIS_DIRECTORY / "src/app/home.py"),],
+        check=True,
+        universal_newlines=True,
+    )
+
+
+@click.command()
+def test_script():
+    subprocess.run(["echo", "Subprocess works --->"])
