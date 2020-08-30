a96a88222e70582b4c474e45ac8040c9786b2b2c
Prathamesh Sarang <prathamesh@difference-engine.ai>
2020-08-30 14:31:48 +0530

    build(scripts): add migrations scripts

--- scripts.py
+++ scripts.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 from pathlib import Path
 
@@ -15,6 +16,18 @@ def st_server():
         check=True,
         universal_newlines=True,
     )
+
+
+@click.command()
+def migrations():
+    logger.info("Running migrations...")
+    os.chdir(THIS_DIRECTORY / "db")
+    subprocess.run(
+        ["poetry", "run", "alembic", "upgrade", "head"],
+        check=True,
+        universal_newlines=True,
+    )
+    logger.info("Completed succeessfully")
 
 
 @click.command()
